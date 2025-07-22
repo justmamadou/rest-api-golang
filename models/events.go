055aea6ef9fc0a9aff75a0669b6268e91200bcc0
MamadouRichatt <mamadou.ba@richatt.com>
2025-07-22 15:06:28 +0000

    Add database integration and routes

--- models/events.go
+++ models/events.go
@@ -2,10 +2,12 @@ package models
 
 import (
 	"time"
+
+	"github.com/justmamadou/rest-api-golang/db"
 )
 
 type Event struct {
-	ID          int
+	ID          int64
 	Name        string
 	Description string
 	Location    string
@@ -15,11 +17,68 @@ type Event struct {
 
 var events = []Event{}
 
-func (e Event) Save() {
+func (e Event) Save() error {
 	// Later: add it to a database
-	events = append(events, e)
+	query := `
+	INSERT INTO events (name, description, location, datetime, user_id)
+	VALUES (?, ?, ?, ?, ?)`
+
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	result, err := stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID)
+	if err != nil {
+		return err
+	}
+
+	id, err := result.LastInsertId()
+	e.ID = id
+	return err
 }
 
-func GetAllEvents() []Event {
-	return events
+func GetAllEvents() ([]Event, error) {
+	query := "SELECT * FROM events"
+	rows, err := db.DB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var events []Event
+	for rows.Next() {
+		var event Event
+		err := rows.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserID)
+		if err != nil {
+			return nil, err
+		}
+		events = append(events, event)
+	}
+	return events, nil
+}
+
+func GetEventByID(id int64) (*Event, error) {
+	query := "SELECT * FROM events WHERE id = ?"
+	row := db.DB.QueryRow(query, id)
+	var event Event
+	err := row.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserID)
+	if err != nil {
+		return nil, err
+	}
+	return &event, nil
+}
+
+func (event Event) UpdateEvent() error {
+	query := "UPDATE events SET name = ?, description = ?, location = ?, datetime = ? WHERE id = ?"
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(event.Name, event.Description, event.Location, event.DateTime, event.ID)
+
+	return err
 }
---
+++ routes/events.go
@@ -0,0 +1,74 @@
+package routes
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/justmamadou/rest-api-golang/models"
+)
+
+func getEvents(c *gin.Context) {
+	events, err := models.GetAllEvents()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Could not retrieve events"})
+		return
+	}
+	c.JSON(http.StatusOK, events)
+}
+
+func createEvent(c *gin.Context) {
+	var event models.Event
+	err := c.ShouldBindJSON(&event)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Could not parse request data"})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+	err = event.Save()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Could not create event"})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"Message": "Successfully created event", "event": event})
+}
+
+func getEvent(c *gin.Context) {
+	eventID, err := strconv.ParseInt(c.Param("id"), 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid event ID"})
+		return
+	}
+
+	event, err := models.GetEventByID(eventID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Could not retrieve event"})
+		return
+	}
+	c.JSON(http.StatusOK, event)
+}
+
+func updateEvent(c *gin.Context) {
+	eventID, err := strconv.ParseInt(c.Param("id"), 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid event ID"})
+		return
+	}
+	_, err = models.GetEventByID(eventID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Could not retrieve event"})
+		return
+	}
+
+	var updateEvent models.Event
+	err = c.ShouldBindJSON(&updateEvent)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Could not parse request data"})
+		return
+	}
+
+}
